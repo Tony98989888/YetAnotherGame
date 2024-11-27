60b8e1a3d9c4304a87a343f4b5f380ee55fe239e
Anthony <649072815@qq.com>
2024-11-27 21:40:28 +0800

    Modified code structure

--- Assets/Scripts/BoxMergeSystem.cs
+++ Assets/Scripts/BoxMergeSystem.cs
@@ -1,28 +1,53 @@
+using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BoxMergeSystem : MonoBehaviour
 {
-    public Box boxPrefab;
-
-    public HashSet<Box> MergingBoxes = new HashSet<Box>();
-
-    private void Start()
-    {
-    }
+    [SerializeField] private Box boxPrefab;
+    
+    private HashSet<Box> mergingBoxes = new HashSet<Box>();
 
     public void MergeBoxes(Box boxA, Box boxB)
     {
-        
-        MergingBoxes.Add(boxA);
-        MergingBoxes.Add(boxB);
+        if (boxA == null || boxB == null)
+        {
+            Debug.Log("Can not merge boxes because boxes is null");
+            return;
+        }
 
-        var spawnPosition = (boxA.transform.position + boxB.transform.position) / 2f;
-        var newBox = Instantiate(boxPrefab, spawnPosition, Quaternion.identity);
-        newBox.Initialize(boxA.Number + boxB.Number);
-        MergingBoxes.Remove(boxA);
-        MergingBoxes.Remove(boxB);
+        if (!mergingBoxes.Add(boxA) || !mergingBoxes.Add(boxB))
+        {
+            Debug.LogWarning("One or more boxes are already in the merging queue.");
+            return;
+        }
+
+        var spawnPosition = CalculateSpawnPosition(boxA, boxB);
+        Box newBox = CreateMergeBox(spawnPosition, boxA.Number + boxB.Number);
+        
+        CleanUpBoxes(boxA, boxB);
+    }
+
+    private Vector3 CalculateSpawnPosition(Box boxA, Box boxB)
+    {
+        return (boxA.transform.position + boxB.transform.position) / 2f;
+    }
+
+    private Box CreateMergeBox(Vector3 spawnPosition, int newNumber)
+    {
+        Box newBox = Instantiate(boxPrefab, spawnPosition, Quaternion.identity);
+        newBox.Initialize(newNumber);
+        return newBox;
+    }
+
+    private void CleanUpBoxes(Box boxA, Box boxB)
+    {
+        mergingBoxes.Remove(boxA);
+        mergingBoxes.Remove(boxB);
+        
         Destroy(boxA.gameObject);
         Destroy(boxB.gameObject);
     }
+
 }
